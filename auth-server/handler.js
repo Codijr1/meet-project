e0aa9022379fc97f6990113c8b3a81e3eb3fff47
Codi <cjrobards@gmail.com>
2024-02-22 17:36:11 -0300

    revert

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -14,12 +14,13 @@ const oAuth2Client = new google.auth.OAuth2(
   redirect_uris[0]
 );
 
-//authorization
 module.exports.getAuthURL = async () => {
+
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
     scope: SCOPES,
   });
+
   return {
     statusCode: 200,
     headers: {
@@ -33,32 +34,21 @@ module.exports.getAuthURL = async () => {
 };
 
 
-//get token
 module.exports.getAccessToken = async (event) => {
   // Decode authorization code extracted from the URL query
   const code = decodeURIComponent(`${event.pathParameters.code}`);
 
-  //debug 
-  console.log('Received authorization code:', code);
-
   return new Promise((resolve, reject) => {
     oAuth2Client.getToken(code, (error, response) => {
       if (error) {
-        // error log
-        console.error('Error exchanging authorization code for token:', error);
         reject(error);
       } else {
-        // response log
-        console.log('Token response:', response);
         resolve(response);
       }
     });
   })
     .then((results) => {
-      // success logged
-      console.log('Token successfully retrieved:', results);
-
-      // OAuth token response
+      // Respond with OAuth token 
       return {
         statusCode: 200,
         headers: {
@@ -69,10 +59,7 @@ module.exports.getAccessToken = async (event) => {
       };
     })
     .catch((error) => {
-      // errors logged
-      console.error('Error retrieving token:', error);
-
-      // error handle/return
+      // Handle error and return response
       return {
         statusCode: 500,
         headers: {
@@ -83,8 +70,6 @@ module.exports.getAccessToken = async (event) => {
       };
     });
 };
-
-//get events
 module.exports.getCalendarEvents = async (event) => {
   return new Promise((resolve, reject) => {
     const access_token = event.queryStringParameters.access_token;

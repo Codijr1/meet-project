c4bc4e6587cf6408504c0a1ec6304b787ccd1396
Codi <cjrobards@gmail.com>
2024-02-27 14:36:50 -0300

    4.4 Halfway

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div id="event-list">
+        Learn React
+      </div>
+    </div>
+  );
+}
+
+export default App;

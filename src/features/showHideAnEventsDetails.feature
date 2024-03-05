cf9597db505393de7cf4543d6ca1712f66e58d31
Codi <cjrobards@gmail.com>
2024-03-05 17:21:22 -0300

    Event.js / showHideAnEventsDetails

---
+++ src/features/showHideAnEventsDetails.feature
@@ -0,0 +1,17 @@
+Feature: A button which shows and then hides an event's details
+
+
+    Scenario: Default Detail Concealment.
+        Given the main page is open,
+        When the app displays a list of events,
+        Then the event details should be hidden by default.
+
+    Scenario: A user wants to show Event details.
+        Given there is an event with hidden details,
+        When the user clicks on the event to show details,
+        Then the app should display the details of the event.
+
+    Scenario: A user wants to hide Event details.
+        Given there is an event with displayed details,
+        When the user clicks on the event to hide details,
+        Then the app should hide the details of the event.
---
+++ src/features/showHideAnEventsDetails.test.js
@@ -0,0 +1,47 @@
+import { defineFeature, loadFeature } from 'jest-cucumber';
+
+const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
+
+defineFeature(feature, (test) => {
+    test('Default Detail Concealment', ({ given, when, then }) => {
+        given('the main page is open', () => {
+
+        });
+
+        when('the app displays a list of events', () => {
+
+        });
+
+        then('the event details should be hidden by default', () => {
+
+        });
+    });
+
+    test('A user wants to show Event details', ({ given, when, then }) => {
+        given('there is an event with hidden details', () => {
+
+        });
+
+        when('the user clicks on the event to show details', () => {
+
+        });
+
+        then('the app should display the details of the event', () => {
+
+        });
+    });
+
+    test('A user wants to hide Event details', ({ given, when, then }) => {
+        given('there is an event with displayed details', () => {
+
+        });
+
+        when('the user clicks on the event to hide details', () => {
+
+        });
+
+        then('the app should hide the details of the event', () => {
+
+        });
+    });
+});

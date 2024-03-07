0cd872988dba6f08c0f2bac63fd199c2cdf39cdb
Codi <cjrobards@gmail.com>
2024-03-07 18:23:58 -0300

    4.6 finish

--- src/features/customEventDisplayLimit.test.js
+++ src/features/customEventDisplayLimit.test.js
@@ -2,14 +2,19 @@ import { loadFeature, defineFeature } from "jest-cucumber";
 import { render, waitFor, within } from "@testing-library/react";
 import React from "react";
 import App from "../App";
+import userEvent from '@testing-library/user-event';
 
 const feature = loadFeature("./src/features/customEventDisplayLimit.feature");
 
 defineFeature(feature, (test) => {
     //scenario 1
     test('Default Event Quantity', async ({ given, when, then }) => {
-        given('the user has not specified the number of events', () => {
-            // No input required
+        let AppComponent;
+        given('the user has not specified the number of events', async () => {
+            AppComponent = render(<App />);
+            await waitFor(() => {
+                AppComponent.container.querySelector('#event-list');
+            });
         });
 
         let eventList;
@@ -32,16 +37,27 @@ defineFeature(feature, (test) => {
 
     //scenario 2
     test('User-Specified Event Quantity', ({ given, when, then }) => {
-        given('the user has specified the number of events', () => {
+        let AppComponent;
+        let numberOfEventsInput;
 
+        given('the user has specified the number of events', async () => {
+            AppComponent = render(<App />);
+            numberOfEventsInput = AppComponent.getByPlaceholderText('#');
+            await userEvent.type(numberOfEventsInput, '32');
         });
 
-        when('the user views the events section', () => {
-            render(<App />);
+        when('the user views the events section', async () => {
+            await waitFor(() => {
+                AppComponent.container.querySelector('#event-list');
+            });
         });
 
-        then('the app should display exactly as many events as specified by the user', () => {
-
+        then('the app should display exactly as many events as specified by the user', async () => {
+            await waitFor(() => {
+                const eventList = AppComponent.container.querySelector('#event-list');
+                const eventListItems = within(eventList).queryAllByRole('listitem');
+                expect(eventListItems.length).toEqual(32);
+            });
         });
     });
 });

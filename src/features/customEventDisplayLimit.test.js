98d187e190d9e223f25205906b59487258750d24
Codi <cjrobards@gmail.com>
2024-03-07 17:52:05 -0300

    4.6 step 5 scenario 1 all passing

--- src/features/customEventDisplayLimit.test.js
+++ src/features/customEventDisplayLimit.test.js
@@ -1,32 +1,39 @@
 import { loadFeature, defineFeature } from "jest-cucumber";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, waitFor, within } from "@testing-library/react";
 import React from "react";
 import App from "../App";
 
 const feature = loadFeature("./src/features/customEventDisplayLimit.feature");
 
 defineFeature(feature, (test) => {
-    // Scenario 1: Default Event Quantity
+    //scenario 1
     test('Default Event Quantity', async ({ given, when, then }) => {
         given('the user has not specified the number of events', () => {
             // No input required
         });
 
+        let eventList;
         when('the user views the events section', async () => {
             render(<App />);
-            await waitFor(() => screen.getByRole('list', { id: 'event-list' }));
+            await waitFor(() => {
+                eventList = document.getElementById('event-list');
+            });
         });
 
-        then('the app should display 32 events by default', () => {
-            const eventList = screen.getByRole('list', { id: 'event-list' });
-            expect(eventList.length) === ('32');
+        then('the app should display 32 events by default', async () => {
+            await waitFor(() => {
+                const eventListItems = within(eventList).queryAllByRole('listitem');
+                expect(eventListItems.length).toBe(32);
+            });
         });
     });
+
 
 
     //scenario 2
     test('User-Specified Event Quantity', ({ given, when, then }) => {
         given('the user has specified the number of events', () => {
+
         });
 
         when('the user views the events section', () => {
@@ -34,6 +41,7 @@ defineFeature(feature, (test) => {
         });
 
         then('the app should display exactly as many events as specified by the user', () => {
+
         });
     });
 });

46a415f6c16f085500f49e5cf629ebf3cc2bd375
Codi <123835607+Codijr1@users.noreply.github.com>
2024-03-17 23:57:43 -0300

    Up to Task

--- src/api.js
+++ src/api.js
@@ -15,8 +15,10 @@ export const extractLocations = (events) => {
 };
 
 export const getEvents = async () => {
-    if (window.location.href.startsWith("http://localhost")) {
-        return mockData[0].items;
+    if (!navigator.onLine) {
+        const events = localStorage.getItem("lastEvents");
+        NProgress.done();
+        return events ? JSON.parse(events) : [];
     }
 
     const token = await getAccessToken();
@@ -27,6 +29,8 @@ export const getEvents = async () => {
         const response = await fetch(url);
         const result = await response.json();
         if (result) {
+            NProgress.done();
+            localStorage.setItem("lastEvents", JSON.stringify(result.events));
             return result.events;
         } else {
             return null;

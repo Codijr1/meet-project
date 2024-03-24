b14db8e876a10d987f9674bce9c19aaebfbff205
Codi <cjrobards@gmail.com>
2024-03-24 12:32:28 -0300

    attempt

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,20 +1,35 @@
-import { render } from '@testing-library/react';
-import NumberOfEvents from '../components/NumberOfEvents.js';
+//number of events 
+import React, { useState } from 'react';
 
-describe('<NumberOfEvents /> component', () => {
-    let NumberOfEventsComponent;
+const NumberOfEvents = ({ setNoe, setErrorText }) => {
+    const [eventNumber, setEventNumber] = useState(32);
 
-    beforeEach(() => {
-        NumberOfEventsComponent = render(<NumberOfEvents />);
-    });
+    const handleInputChange = (event) => {
+        const value = event.target.value;
+        if (value === '' || isNaN(value) || value <= 0 || value > 32) {
+            setErrorText('Number of Events must be greater than 0');
+            setEventNumber('32');
+            setNoe(32);
+        } else {
+            setErrorText('');
+            const parsedValue = parseInt(value, 10);
+            setEventNumber(parsedValue);
+            setNoe(parsedValue);
+        }
+    };
 
-    test('has an element with "spinbutton" role', () => {
-        expect(NumberOfEventsComponent.queryByRole("spinbutton")).toBeInTheDocument();
-    });
+    return (
+        <div id="numberOfEvents">
+            <input
+                type="number"
+                value={eventNumber}
+                onChange={handleInputChange}
+                min="1"
+                max="32"
+                placeholder="#"
+            />
+        </div>
+    );
+};
 
-    test('default value is 32', () => {
-        const inputValue = NumberOfEventsComponent.queryByRole('spinbutton').value;
-        expect(String(inputValue)).toBe('32');
-    });
-
-});
+export default NumberOfEvents;
